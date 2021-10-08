039d7af8550dc2efa2c1ea7fd383abd66d5fd787
Ellawyn Fong <ellawynfong@gmail.com>
2021-10-07 22:52:36 -0400

    Added the pixel art for the character, character SHOULD flip back and forth, could not test because feet had not been assigned.

--- New Unity Project (3)/Assets/Code/LifeSpan.cs
+++ New Unity Project (3)/Assets/Code/LifeSpan.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LifeSpan : MonoBehaviour
 {
     public int lifeSeconds = 2;
     void Start()
     {
-        Destroy(GameObject, LifeSeconds);
+        Destroy(this, lifeSeconds);
     }
 }
