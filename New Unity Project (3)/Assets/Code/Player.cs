77a9baa266d309d1809cc5ddb9e6572b3060580a
kc3753 <47250615+kc3753@users.noreply.github.com>
2021-10-14 10:46:07 -0400

    started level2 scene camera follow

--- New Unity Project (3)/Assets/Code/Player.cs
+++ New Unity Project (3)/Assets/Code/Player.cs
@@ -7,7 +7,8 @@ using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
     public int speed = 4;
-    public int jumpForce = 500;
+    public static int jumpForce = 500;
+    public static int numjumps = 1;
     public int bulletForce = 800;
     public LayerMask groundLayer;
     public Transform feet;
@@ -18,12 +19,11 @@ public class Player : MonoBehaviour
     Rigidbody2D _rigidbody;
 
     float xSpeed = 0;
-    int jumps = 0;
+    int jumps;
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
-
 
     void FixedUpdate()
     {
@@ -39,7 +39,7 @@ public class Player : MonoBehaviour
     {
         grounded = Physics2D.OverlapCircle(feet.position, .3f, groundLayer);
         if(grounded){
-            jumps = 1;
+            jumps = numjumps;
         }
         if(Input.GetButtonDown("Jump")&& (jumps > 0 || grounded))
         {
@@ -47,21 +47,29 @@ public class Player : MonoBehaviour
             _rigidbody.AddForce(new Vector2(0,jumpForce));
             jumps--;
         }
+        /*
         if(Input.GetButtonDown("Fire1"))
         {
             GameObject newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
             newBullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(bulletForce,0));
         }
+        */
         if (Input.GetKeyDown(KeyCode.K)){
             SceneManager.LoadScene("GameStart");
         }
+        if(NextLevel.levelToLoad == 2){
+            if(feet.position.y <= -10){
+                SceneManager.LoadScene("Level 2");
+            }
+        }
     }
-
+/*
     void OnTriggerEnter2D(Collider2D other){
         if (other.CompareTag("Gate")){
             print("Move to next level.");
         }
     }
+    */
     void OnCollisionEnter2D(Collision2D other){
         if (other.gameObject.tag == "Balloon"){
             balloonCount++;
