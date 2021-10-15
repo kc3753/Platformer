6c7fd8b6c1a1a59fa5aaeec105c9b5ce5a788f91
Ellawyn Fong <ellawynfong@gmail.com>
2021-10-15 11:55:55 -0400

    merge problems

---
+++ New Unity Project (3)/Assets/Code/Fall.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fall : MonoBehaviour
+{
+    Rigidbody2D _rigidbody;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player")){
+            _rigidbody.isKinematic = false;
+        }
+    }
+}
