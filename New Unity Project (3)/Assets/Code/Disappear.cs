2f68dcd03c5d9b5fd89fdf797f24f8c98c332e08
kc3753 <47250615+kc3753@users.noreply.github.com>
2021-10-20 21:55:30 -0400

    created disappearing platforms

---
+++ New Unity Project (3)/Assets/Code/Disappear.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear : MonoBehaviour
+{
+    public GameObject platform;
+    public float currTime = 0;
+    public bool enable = true;
+    public float timeToDisappear;
+    void Start() {
+        timeToDisappear = 4f + Random.Range(-1.0f,1.0f);
+    }
+    
+    void Update()
+    {
+        currTime += Time.deltaTime;
+        if(currTime >= timeToDisappear){
+            if(enable){
+              platform.SetActive(false); 
+              enable = false; 
+            }
+            else{
+            platform.SetActive(true);
+            enable = true;
+            }
+            currTime = 0;
+        }
+    }
+}
